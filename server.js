ea059bbc1538c8eebf114736e78b8e925d2aa2dc
Shubham Jaiswal <greatshubhamjaiswal1@gmail.com>
2017-06-24 18:22:06 +0530

    Three Article urls are added

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function(req, res){
+	res.send('Article one is Requested');
+});
+
+app.get('/article-two', function(req,res){
+	res.send('Article Two is Requested');
+});
+
+app.get('/article-three', function(req,res){
+	res.send('Article three is requested');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
