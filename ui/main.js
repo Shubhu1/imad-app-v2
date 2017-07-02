20427cc7bf61e52537fdc247162162a99f32a235
Shubhu1 <greatshubhamjaiswal1@gmail.com>
2017-07-02 19:17:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+var clicked = document.getElementById("btn");
+
+clicked.onclick = function()
+{
+    counter =  counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+};
