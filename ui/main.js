068bcf180a81a1aa36b6b7d5a36ddf90d21906d7
Shubhu1 <greatshubhamjaiswal1@gmail.com>
2017-07-02 19:20:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 var clicked = document.getElementById("btn");
-
+var counter =0;
 clicked.onclick = function()
 {
-    counter =  counter + 1;
+     counter =  counter + 1;
     var span = document.getElementById("count");
     span.innerHTML = counter.toString();
 };
